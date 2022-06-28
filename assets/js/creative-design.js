1465c01633a8a90ba7bec88ce04fe6836a3c4b70
Derrick Agorhom <76208189+derekagorhom@users.noreply.github.com>
2022-06-28 16:19:36 +0200

    changes

--- assets/js/creative-design.js
+++ assets/js/creative-design.js
@@ -48,3 +48,18 @@ function myFunction() {
     }
   } 
 
+  function myFunction() {
+    var dots = document.getElementById("dot");
+    var moreText = document.getElementById("More");
+    var btnText = document.getElementById("Btn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  } 
